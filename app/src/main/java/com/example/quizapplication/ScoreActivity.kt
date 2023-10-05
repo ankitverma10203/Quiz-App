8fccddb830433520861092cc9ebba74214ef3322
ankitverma10203 <43405291+ankitverma10203@users.noreply.github.com>
2023-10-05 22:56:12 +0530

    add finish button on final score screen

--- app/src/main/java/com/example/quizapplication/ScoreActivity.kt
+++ app/src/main/java/com/example/quizapplication/ScoreActivity.kt
@@ -1,13 +1,16 @@
 package com.example.quizapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class ScoreActivity : AppCompatActivity() {
 
     var congratsTv : TextView? = null
     var scoreTv : TextView? = null
+    var finishBtn : Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_score)
@@ -16,5 +19,13 @@ class ScoreActivity : AppCompatActivity() {
         congratsTv?.text = "Congrats ${intent.getStringExtra(Constants.USER_NAME_KW)}!"
         scoreTv = findViewById(R.id.score)
         scoreTv?.text = "Score: ${intent.getIntExtra(Constants.SCORE_KW, 0)}/${intent.getIntExtra(Constants.TOTAL_QUESTIONS_KW, 0)}"
+
+        finishBtn = findViewById<Button>(R.id.finish)
+        finishBtn?.setOnClickListener {
+            val intent : Intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
     }
 }
